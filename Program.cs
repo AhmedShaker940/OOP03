341c238f4a39d556fec3282a74963a83525947dc
AhmedShaker940 <a7medsar7an123@gmail.com>
2026-08-11 06:53:03 +0300

    Question01-Theoretical Questoins

--- Program.cs
+++ Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
+            #region Part 01 — Theoretical Questions
 
+            #region Question01
+            //Q1  Overloading, Overriding, and Binding
+            //a)  What is the difference between Method Overloading and Method Overriding?
+            /*
+             Method Overloading Allow the class to have multible Methods with the same name but with different parameters (One Method with multible tasks)
+             Method Overriding the method is already exists in the base class (Parent) but with different implementation in the derived class (Child) (same Method with different implementation)
+             */
+
+            //b)  What is the difference between Static Binding and Dynamic Binding?
+            /*
+             Static Binding is a technique that happens in the compile time by compiler , Faster than Dynamic Binding , Referance always wins , (Method overloading , static methods , method hiding)
+             Dynamic Binding is a technique that happens in run time by CLR , Slower than Static Binding , The object wins , (Method Overriding, Abstract Methods) )
+             */
+            #endregion
+
+            #endregion
         }
     }
 }
