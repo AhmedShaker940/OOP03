c59404f4a0417b7fd05dc009a44a2395ed809f4d
AhmedShaker940 <a7medsar7an123@gmail.com>
2026-08-11 07:01:11 +0300

    Question-Theoretical Questions

--- Program.cs
+++ Program.cs
@@ -21,6 +21,22 @@
              */
             #endregion
 
+            #region Question02
+            //Q2  Sealed Classes and Methods
+            //a)  What is the purpose of the sealed keyword when applied to a class?
+            /*
+             Sealed keyword when applied to a class makes the class can not be inherited we use it mainly to protect senstive data (there is no inheritance chain)
+             */
+            //b)  What is the difference between a sealed class and a sealed method? 
+            /*
+             sealed class can not be inherited at all , sealed method prevent the further override of a specific method 
+             */
+            //c)  Can a sealed method be overridden? Why?
+            /*
+             No , A Sealed Method can't be overridden After using sealed keyword because it means that this method override is final and can not be overridden further 
+             */
+            #endregion
+
             #endregion
         }
     }
